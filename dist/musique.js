9720485e511fe3277a9850503be71aff335db9f0
Guille Paz <guille87paz@gmail.com>
2014-02-27 00:44:04 -0300

    Create musique.js files.

---
+++ dist/musique.js
@@ -0,0 +1,272 @@
+/*!
+ * musique.js v1.0.0
+ *
+ * Copyright (c) 2014, @pazguille
+ * Released under the MIT license.
+ */
+(function (window) {
+    'use strict';
+
+    var defaults = {
+        'volume': 50,
+        'indexTrack': 0,
+        'muted': false,
+        'playlist': [],
+        'autoplay': false
+    };
+
+    function customizeOptions(options) {
+        var prop;
+        for (prop in defaults) {
+            if (!options.hasOwnProperty(prop)) {
+                options[prop] = defaults[prop];
+            }
+        }
+        return options;
+    }
+
+    /**
+     * Creates a new instance of Musique.
+     * @constructor
+     * @returns {musique}
+     */
+    function Musique(options) {
+        this._init(options);
+    }
+
+    /**
+     * Initializes a new instance of Musique.
+     * @memberof! Musique.prototype
+     * @function
+     * @private
+     * @returns {musique}
+     */
+    Musique.prototype._init = function (options) {
+
+        this._options = customizeOptions(options || {});
+
+        this._player = new window.Audio();
+
+        this._muted = this._options.muted;
+
+        this._indexTrack = this._options.indexTrack;
+
+        this.volume(this._options.volume);
+
+        this.playlist = this._options.playlist;
+
+        this.setTrack(this.playlist[this._indexTrack]);
+
+        this._defineEvents();
+
+        if (this._options.autoplay) {
+            this._player.play();
+        }
+
+        return this;
+    };
+
+    /**
+     * Starts playback the current track or a given track.
+     * @memberof! Musique.prototype
+     * @function
+     * @returns {musique}
+     */
+    Musique.prototype.play = function (track) {
+        if (track) {
+            this.setTrack(track);
+        }
+
+        this._player.play();
+
+        return this;
+    };
+
+    /**
+     * Sets a given track as current track.
+     * @memberof! Musique.prototype
+     * @function
+     * @returns {musique}
+     */
+    Musique.prototype.setTrack = function (track) {
+
+        this.currentTrack = track;
+        this._player.src = track.src;
+
+        return this;
+    };
+
+    /**
+     * Pauses playback.
+     * @memberof! Musique.prototype
+     * @function
+     * @returns {musique}
+     */
+    Musique.prototype.pause = function () {
+        this._player.pause();
+
+        return this;
+    };
+
+    /**
+     * Stops playback and seeks to the beginning.
+     * @memberof! Musique.prototype
+     * @functionMusique
+     * @returns {musique}
+     */
+    Musique.prototype.stop = function () {
+        if (this._player.currentTime) {
+            this._player.currentTime = 0;
+            this._player.pause();
+        }
+
+        return this;
+    };
+
+    /**
+     * Jumps to the next track.
+     * @memberof! Musique.prototype
+     * @function
+     * @returns {musique}
+     */
+    Musique.prototype.next = function () {
+        var index = this._indexTrack + 1;
+
+        if (index >= this.playlist.length) {
+            return this;
+        }
+
+        this._indexTrack = index;
+        this.setTrack(this.playlist[this._indexTrack]);
+
+        if (!this._player.paused) {
+            this.play();
+        }
+
+        return this;
+    };
+
+    /**
+     * Jumps to the previous track.
+     * @memberof! Musique.prototype
+     * @function
+     * @returns {musique}
+     */
+    Musique.prototype.prev = function () {
+        var index = this._indexTrack - 1;
+
+        if (index < 0) {
+            return this;
+        }
+
+        this._indexTrack = index;
+        this.setTrack(this.playlist[this._indexTrack]);
+
+        if (!this._player.paused) {
+            this.play();
+        }
+
+        return this;
+    };
+
+    /**
+     * Defines the volumen as a value from 0 to 100.
+     * @memberof! Musique.prototype
+     * @function
+     * @returns {musique}
+     */
+    Musique.prototype.volume = function (value) {
+
+        this._player.volume = value / 100;
+
+        return this;
+    };
+
+    /**
+     * Mutes an instance of Musique.
+     * @memberof! Musique.prototype
+     * @function
+     * @returns {musique}
+     */
+    Musique.prototype.mute = function () {
+        this._muted = !this._muted;
+        this._player.muted = this._muted;
+
+        return this;
+    };
+
+    /**
+     * Reset the current track to the initial.
+     * @memberof! Musique.prototype
+     * @function
+     * @returns {musique}
+     */
+    Musique.prototype.reset = function () {
+        this._indexTrack = this._options.indexTrack;
+        this.stop();
+
+        return this;
+    };
+
+    /**
+     * Toggles between pause and play..
+     * @memberof! Musique.prototype
+     * @function
+     * @returns {musique}
+     */
+    Musique.prototype.toggle = function () {
+        if (this._player.paused) {
+            this.play();
+        } else {
+            this.pause();
+        }
+
+        return this;
+    };
+
+    /**
+     * Defines DOM events to Audio instance.
+     * @memberof! Musique.prototype
+     * @function
+     * @private
+     * @returns {musique}
+     */
+    Musique.prototype._defineEvents = function () {
+        var that = this,
+            player = this._player;
+
+        player.addEventListener('ended', function (eve) {
+            var track = that.currentTrack;
+
+            that.next();
+
+            if (track !== that.currentTrack) {
+                that.play();
+            } else {
+                that.reset();
+            }
+
+        });
+
+        return this;
+    };
+
+    /**
+     * Expose Musique
+     */
+    // AMD suppport
+    if (typeof window.define === 'function' && window.define.amd !== undefined) {
+        window.define('Musique', [], function () {
+            return Musique;
+        });
+
+    // CommonJS suppport
+    } else if (typeof module !== 'undefined' && module.exports !== undefined) {
+        module.exports = Musique;
+
+    // Default
+    } else {
+        window.Musique = Musique;
+    }
+
+}(this));
